852fe1c6af11a938fecace6183bac648df4c0bcd
Jose Andres González Ventura <jose_x_744@hotmail.com>
2018-03-06 13:18:15 +0100

    Refactorizando el test

---
+++ test/org/eda2/practica1/TestPrueba.java
@@ -0,0 +1,14 @@
+package org.eda2.practica1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestPrueba {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
