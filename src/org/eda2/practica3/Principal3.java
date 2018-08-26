489581a06720fb0feb43f5ee10fd78bff3e9969a
Francisco Javier Marques Gaona <fmg703@inlumine.ual.es>
2018-08-26 17:42:54 +0200

    Entregables

--- src/org/eda2/practica3/Principal3.java
+++ src/org/eda2/practica3/Principal3.java
@@ -11,7 +11,7 @@ import java.util.TreeMap;
 public class Principal3 {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		String directorio = System.getProperty("user.dir")
 				+File.separator + "src"+File.separator
 				+"datosCiudad" + File.separator;
@@ -20,12 +20,18 @@ public class Principal3 {
 				+"tablaP3" + File.separator;
 		String nombreFichero = directorio + "ciudad2B.txt";
 		String nombreTabla = directorio2 + "tablaPrecios.txt";
-		
+		long antes, despues, tiempo;
+
 		File f = new File(nombreTabla);
 		BufferedWriter bw = new BufferedWriter(new FileWriter(f));
 		TablasPrecios grafo = new TablasPrecios (nombreFichero);
 		//grafo.escribirTablas();
+		antes = System.nanoTime();
 		String resultado = grafo.escribirTablas();
+		despues = System.nanoTime();
+		tiempo = despues - antes;
+		System.out.println();
+		System.out.println("Tiempo que se tarda en calcular las tablas:" + tiempo);
 		bw.write(resultado);
 		bw.close();
 		}
